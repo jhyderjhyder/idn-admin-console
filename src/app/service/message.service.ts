029ec57adca6ed49c1b9a50beeb52599341e1a4e
Kenny Li <kenny.li@sailpoint.com>
2022-01-10 21:18:57 +0800

    added error status code check

--- src/app/service/message.service.ts
+++ src/app/service/message.service.ts
@@ -25,12 +25,21 @@ export class MessageService {
 
     handleIDNError(errResponse) {
       let errMsg = "";
+      let errStatusCode = errResponse.status;
+
       if (errResponse.error) {
-        if (errResponse.error.messages && errResponse.error.messages.length > 0 && errResponse.error.messages[0].text) {
-          errMsg += "Error message: " + errResponse.error.messages[0].text;
+        if (errStatusCode === 400 || errStatusCode === 403 || errStatusCode === 404 || errStatusCode === 500) {
+          if (errResponse.error.messages && errResponse.error.messages.length > 0 && errResponse.error.messages[0].text) {
+            errMsg += "Error message: " + errResponse.error.messages[0].text;
+          }
+          if (errResponse.error.detailCode) {
+            errMsg += " (Error code: " + errResponse.error.detailCode + ")";
+          }
         }
-        if (errResponse.error.detailCode) {
-          errMsg += " (Error code: " + errResponse.error.detailCode + ")";
+        else if (errStatusCode === 429) {
+          if (errResponse.error.message) {
+            errMsg += "Error message: " + errResponse.error.message;
+          }
         }
       } else {
         errMsg += "Error to invoke IDN API. Please contact the system adminstrator.";
