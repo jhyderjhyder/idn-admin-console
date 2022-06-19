bc2e2a0114360373a563314296109cf664d49fca
piyush-khandelwal-sp <piyush.khandelwal@sailpoint.com>
2022-06-19 16:01:35 +1000

    Enable Actions and Remove popper
    - Enable Github Actions for CI/CD Builds
    - Remove popper dependency to fix buble build issue
    - Update ReadMe

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'health-ecare-ui'`, () => {
+  it(`should have as title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('health-ecare-ui');
+    expect(app.title).toEqual('title');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to health-ecare-ui!');
+    expect(compiled.querySelector('h1').textContent).toContain('');
   });
 });
