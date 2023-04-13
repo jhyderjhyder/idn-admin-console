47041f6634adc442da6f88c3d32124bd9540e3a6
piyush-khandelwal-sp <piyush.khandelwal@sailpoint.com>
2023-04-14 08:43:27 +1000

    Export All - LCS & Source Create Profiles

--- src/app/identity-lcs-management/identity-lcs-management.component.ts
+++ src/app/identity-lcs-management/identity-lcs-management.component.ts
@@ -6,6 +6,7 @@ import { MessageService } from '../service/message.service';
 import * as JSZip from 'jszip';
 import { saveAs } from 'file-saver';
 import { IdentityProfile } from '../model/identity-profile';
+import { AuthenticationService } from '../service/authentication-service.service';
 
 @Component({
   selector: 'app-identity-lcs-management',
@@ -32,7 +33,8 @@ export class IdentityLCSComponent implements OnInit {
 
   constructor(
     private idnService: IDNService,
-    private messageService: MessageService
+    private messageService: MessageService,
+    private authenticationService: AuthenticationService
   ) {}
 
   ngOnInit() {
@@ -151,42 +153,51 @@ export class IdentityLCSComponent implements OnInit {
       );
   }
 
-  //  exportAllIdentityProfilesLCS() {
+  exportAllIdentityProfilesLCS() {
+    this.idnService.getAllIdentityProfiles().subscribe(
+      results => {
+        this.identityProfiles = [];
+        const promises = [];
 
-  //   this.idnService.getAllIdentityProfiles()
-  //         .subscribe(
-  //           results => {
-  //           this.identityProfiles = [];
-  //           for (const each of results) {
-  //             const identityProfile = new IdentityProfile();
+        for (const each of results) {
+          const identityProfile = new IdentityProfile();
+          identityProfile.name = each.name;
+          identityProfile.id = each.id;
 
-  //             identityProfile.name = each.name;
-  //             identityProfile.id = each.id;
+          const promise = this.idnService
+            .getIdentityProfileLCS(identityProfile.id)
+            .toPromise()
+            .then(
+              result => {
+                result = JSON.stringify(result, null, 4);
+                const fileName = 'LCS - ' + identityProfile.name + '.json';
+                this.zip.file(`${fileName}`, result);
+              },
+              err => {
+                if (err.status === 404) {
+                  console.log(
+                    `No LCS profile found for Identity Profile ${identityProfile.name}`
+                  );
+                } else {
+                  this.messageService.handleIDNError(err);
+                }
+              }
+            );
+          promises.push(promise);
+        }
 
-  //             this.idnService.getIdentityProfileLCS(identityProfile.id)
-  //             .subscribe(
-  //               result => {
-  //                 result = JSON.stringify(result, null, 4);
-
-  //                 const fileName = "LCS - " + identityProfile.name + ".json";
-  //                 this.zip.file(`${fileName}`, result);
-
-  //               },
-  //               err => this.messageService.handleIDNError(err)
-  //             );
-
-  //           }
-  //           const currentUser = this.authenticationService.currentUserValue;
-  //           const zipFileName = `${currentUser.tenant}-identityprofiles-lcs.zip`;
-
-  //          this.zip.generateAsync({type:"blob"}).then(function(content) {
-  //             saveAs(content, zipFileName);
-  //         });
-
-  //         this.ngOnInit();
-
-  //         });
-  // }
+        Promise.all(promises).then(() => {
+          const currentUser = this.authenticationService.currentUserValue;
+          const zipFileName = `${currentUser.tenant}-identityprofiles-lcs.zip`;
+          this.zip.generateAsync({ type: 'blob' }).then(content => {
+            saveAs(content, zipFileName);
+          });
+          this.ngOnInit();
+        });
+      },
+      err => this.messageService.handleIDNError(err)
+    );
+  }
 
   showDeleteLCSSubmitConfirmModal(lcsId: string) {
     this.messageService.clearError();
