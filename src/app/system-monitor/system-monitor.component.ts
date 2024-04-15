bb8a0b4811fb95a4a0b48471aec97f40d84fa5fd
Jed <jed.hyder@exxonmobil.com>
2024-04-15 09:33:25 -0500

    added export function for va report

--- src/app/system-monitor/system-monitor.component.ts
+++ src/app/system-monitor/system-monitor.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { VaConfig } from '../model/va-config';
 //import { MessageService } from '../service/message.service';
 import { IDNService } from '../service/idn.service';
+import * as JSZip from 'jszip';
+import { saveAs } from 'file-saver';
+import { AuthenticationService } from '../service/authentication-service.service';
+import { AngularCsv } from 'angular-csv-ext/dist/Angular-csv';
 
 @Component({
   selector: 'app-system-monitor',
@@ -11,11 +15,19 @@ import { IDNService } from '../service/idn.service';
 export class SystemMonitorComponent implements OnInit {
   cluster: VaConfig[];
   loading: boolean;
-  vaIssues: number;
-  clusterIssues: number;
+
+  vaGood: number;
+  vaWarning: number;
+  vaError: number;
+
+  clusterError: number;
+  clusterGood: number;
+  clusterWarning: number;
+  zip: JSZip = new JSZip();
 
   constructor(
-    private idnService: IDNService // private messageService: MessageService
+    private idnService: IDNService,
+    private authenticationService: AuthenticationService
   ) {}
   ngOnInit() {
     console.log('Started Monitor');
@@ -24,20 +36,33 @@ export class SystemMonitorComponent implements OnInit {
 
   getStatus() {
     this.cluster = [];
-    this.clusterIssues = 0;
-    this.vaIssues = 0;
+
+    this.clusterError = 0;
+    this.clusterGood = 0;
+    this.clusterWarning = 0;
+
+    this.vaGood = 0;
+    this.vaError = 0;
+    this.vaWarning = 0;
+
     this.loading = true;
     this.idnService.getAllVAClusters().subscribe(async clusters => {
       for (const each of clusters) {
-        if (each.status != 'NORMAL') {
-          this.clusterIssues++;
+        if (each.status == 'NORMAL') {
+          this.clusterGood++;
+        } else {
+          if (each.status != 'WARNING') {
+            this.clusterWarning++;
+          } else {
+            this.clusterError++;
+          }
         }
 
         this.idnService
           .getClusterDetails(each.id)
           .subscribe(async clusterDetails => {
             if (clusterDetails.clientsStatus.length == 0) {
-              var c = new VaConfig();
+              const c = new VaConfig();
               c.id = each.id;
               c.name = each.name;
               c.ccgVersion = each.ccgVersion;
@@ -47,7 +72,7 @@ export class SystemMonitorComponent implements OnInit {
               this.cluster.push(c);
             }
             for (const d of clusterDetails.clientsStatus) {
-              var c = new VaConfig();
+              const c = new VaConfig();
               c.id = each.id;
               c.name = each.name;
               c.ccgVersion = each.ccgVersion;
@@ -58,8 +83,15 @@ export class SystemMonitorComponent implements OnInit {
               c.clientIp = d.internalIp;
               c.clientHostName = d.hostname;
               c.clientVersion = d.ccgVersion;
-              if (c.clientStatus != 'NORMAL') {
-                this.vaIssues++;
+              if (c.clientStatus == 'NORMAL') {
+                this.vaGood++;
+              } else {
+                if (c.clientStatus == 'WARN') {
+                  //Not sure if clients have warnings
+                  this.vaWarning++;
+                } else {
+                  this.vaError++;
+                }
               }
               this.cluster.push(c);
             }
@@ -68,5 +100,53 @@ export class SystemMonitorComponent implements OnInit {
 
       this.loading = false;
     });
+  }
+
+  download() {
+    const jsonData = JSON.stringify(this.cluster, null, 4);
+    const fileName = 'vaDetails.json';
+    this.zip.file(`${fileName}`, jsonData);
+
+    const currentUser = this.authenticationService.currentUserValue;
+    const zipFileName = `${currentUser.tenant}-vaDetails.zip`;
+
+    this.zip.generateAsync({ type: 'blob' }).then(function (content) {
+      saveAs(content, zipFileName);
+    });
+  }
+
+  saveInCsv() {
+    const options = {
+      fieldSeparator: ',',
+      quoteStrings: '"',
+      decimalseparator: '.',
+      showLabels: true,
+      useHeader: true,
+      headers: [
+        'id',
+        'name',
+        'ccgVersion',
+        'status',
+        'description',
+        'clientid',
+        'clientStatus',
+        'clientHypervisor',
+        'clientVersion',
+        'clientIp',
+        'clientHostName',
+      ],
+      nullToEmptyString: true,
+    };
+
+    const currentUser = this.authenticationService.currentUserValue;
+    const fileName = `${currentUser.tenant}-vaDetails`;
+
+    const arr = [];
+    for (const each of this.cluster) {
+      const record = Object.assign(each);
+      arr.push(record);
+    }
+
+    new AngularCsv(arr, fileName, options);
   }
 }
